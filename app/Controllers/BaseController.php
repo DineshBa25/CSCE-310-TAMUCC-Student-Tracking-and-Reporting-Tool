9e2eaad09796963f0721840ae1f959b74f298152
dineshba25 <dineshba63@gmail.com>
2023-12-03 21:43:09 -0600

    made base controller contain user data globally

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -1,58 +1,28 @@
 <?php
+// File: app/Controllers/BaseController.php
 
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use CodeIgniter\HTTP\CLIRequest;
-use CodeIgniter\HTTP\IncomingRequest;
-use CodeIgniter\HTTP\RequestInterface;
-use CodeIgniter\HTTP\ResponseInterface;
-use Psr\Log\LoggerInterface;
 
-/**
- * Class BaseController
- *
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- * Extend this class in any new controllers:
- *     class Home extends BaseController
- *
- * For security be sure to declare any new methods as protected or private.
- */
-abstract class BaseController extends Controller
+class BaseController extends Controller
 {
-    /**
-     * Instance of the main Request object.
-     *
-     * @var CLIRequest|IncomingRequest
-     */
-    protected $request;
+    protected $userData; // Declare a property to hold user data
 
-    /**
-     * An array of helpers to be loaded automatically upon
-     * class instantiation. These helpers will be available
-     * to all other controllers that extend BaseController.
-     *
-     * @var array
-     */
-    protected $helpers = [];
-
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
-    // protected $session;
-
-    /**
-     * @return void
-     */
-    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
     {
         // Do Not Edit This Line
         parent::initController($request, $response, $logger);
 
-        // Preload any models, libraries, etc, here.
+        // Check if user is logged in
+        if (session()->get('isLoggedIn')) {
+            // Assuming you have the username stored in the session or request
+            $username = session()->get('username');
 
-        // E.g.: $this->session = \Config\Services::session();
+            // Fetch user data and store it in the property
+            $model = new \App\Models\UserModel();
+            $this->userData = $model->where('username', $username)->first();
+        }
     }
 }
+
